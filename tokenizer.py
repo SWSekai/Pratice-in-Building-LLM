bfbb09fecd7248142958a2889aefef3676be89c1
Hans Chen <hanschen31310@gmail.com>
2025-08-02 00:55:54 +0800

    08/01 Rename class name, add specific token

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,6 @@
 import re
 
-class SimpletokenizerV1:
+class SimpleTokenizer:
     def __init__(self, vocab):
         self.str_to_int = vocab
         self.int_to_str = {i: s for s, i in vocab.items()}
@@ -9,9 +9,12 @@ class SimpletokenizerV1:
         """
             text to token ID
         """
-        tokens = re.split(r'([,.:;?_!"()\']|--|\s)', text) # 使用正則表達式分割文本
-        preprocessed_text = [token for token in tokens if token.strip()] # 過濾掉空字符串
-        ids = [self.str_to_int[s] for s in preprocessed_text]
+        preprocessing_text = re.split(r'([,.:;?_!"()\']|--|\s)', text) # 使用正則表達式分割文本
+        preprocessed_text = [token for token in preprocessing_text if token.strip()] # 過濾掉空字符串
+        tokens = [item if item in self.str_to_int
+                                else "<|unk|>" for item in preprocessed_text] # 將未知詞替換為 <|unk|>
+        
+        ids = [self.str_to_int[s] for s in tokens]
         
         return ids
     
@@ -23,3 +26,6 @@ class SimpletokenizerV1:
         text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)  # 去除標點符號前多餘空格
         
         return text.strip()  # 去除首尾空格
+    
+if __name__ == "__main__":
+    print("Execute Successfully")
