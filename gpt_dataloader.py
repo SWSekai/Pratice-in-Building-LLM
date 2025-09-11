b767d99f805a7e7ad9ad54cee8e1b53d87ea5a2f
Hans Chen <hanschen31310@gmail.com>
2025-09-11 19:05:22 +0800

    09/11 modify test content and annotation

--- gpt_dataloader.py
+++ gpt_dataloader.py
@@ -4,9 +4,10 @@ from torch.utils.data import Dataset, DataLoader
 import tiktoken
 from preprocessed_txt import Txt_preprocessor
 
+import numpy as np
 class GPTDataset(Dataset):
     """
-        批次載入自訂資料集
+        自訂資料集(批次載入)
     """
     def __init__(self, txt, tokenizer, max_length, stride):
         self.input_ids = []
@@ -36,17 +37,17 @@ class GPTDataset(Dataset):
         return self.input_ids[idx], self.target_ids[idx]
 
 def create_dataloader(txt, 
-                      batch_size= 4, 
-                      max_length= 256,
-                      stride= 128, 
+                      batch_size= 4,  # 批次大小(每次處理資料筆數)
+                      max_length= 256, # 輸入資料和預測目標的最大長度(每筆資料量大小)
+                      stride= 128, # y 資料流的偏移量
                       shuffle= True,
                       drop_last= True,
                       num_worker= 0):
     """
-        批次載入資料載入器
+        資料載入器(批次載入)
     """
     tokenizer = tiktoken.get_encoding("gpt2") # 使用GPT-2的預訓練分詞器
-    dataset = GPTDataset(txt, tokenizer, max_length, stride) # 建立自訂資料集
+    dataset = GPTDataset(txt, tokenizer, max_length, stride)
     data_loader = DataLoader(dataset, 
                             batch_size= batch_size, 
                             shuffle= shuffle, 
@@ -59,7 +60,9 @@ def create_dataloader(txt,
 
 if __name__ == "__main__":
     preprocessed_txt = Txt_preprocessor()
-    data_loader = create_dataloader(preprocessed_txt.rawText, batch_size= 1, max_length= 4, stride= 1, shuffle= False)
+    data_loader = create_dataloader(preprocessed_txt.rawText, batch_size= 8, max_length= 4, stride= 4, shuffle= False)
+    
     data_iter = iter(data_loader) # 建立資料迭代器
-    first_batch = next(data_iter) # 取得第一個批次
-    print(first_batch)
+    input, target = next(data_iter) # 取得第一個批次
+    print(f"Data Shape: {input.shape}") # 輸入資料形狀
+    print(f"Input:\n{input},\n\nTarget:\n{target}")
