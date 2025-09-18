485c22aabb75480af5ef76e77d872f79542f2089
Hans Chen <hanschen31310@gmail.com>
2025-09-18 16:44:42 +0800

    09/18 functionalization

--- embedding_layer.py
+++ embedding_layer.py
@@ -1,20 +1,23 @@
 import torch
 from gpt_dataloader import create_dataloader
+
 from text_processing import Txt_preprocessor
 
-def create_embedding_layer(vocab_size= 50257, embedding_dim= 256):
+def token_embedding(token_id, vocab_size, embedding_dim):
     """
-        建立詞嵌入層
+        詞嵌入
     """
     token_embedding_layer = torch.nn.Embedding(vocab_size, embedding_dim) # 建立詞嵌入層
+    token_embeddings = token_embedding_layer(token_id) # 將token_id轉換為詞嵌入向量
     
-    return token_embedding_layer
+    return token_embeddings
 
 if __name__ == "__main__":
     vocab_size = 50257  # GPT-2的詞彙表大小
     embedding_dim = 256 # 詞嵌入維度
 
     raw_text = Txt_preprocessor().rawText
+    
     data_loader = create_dataloader(
         raw_text,
         batch_size= 8,
@@ -23,4 +26,9 @@ if __name__ == "__main__":
         shuffle= False
     )
     
-    create_embedding_layer(vocab_size, embedding_dim)
+    data_iter = iter(data_loader) # 建立資料迭代器
+    input, target = next(data_iter) # 取得第一個批次
+    print("First batch input token ID: ", input)
+    
+    token_embeddings = token_embedding(input, vocab_size, embedding_dim)
+    print(token_embeddings.shape)
